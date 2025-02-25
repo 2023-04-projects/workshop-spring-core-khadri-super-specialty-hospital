d5b52a23103fdf4afed481b8975de270c25e15cb
khadri2021 <khadri.institutions@gmail.com>
2025-02-25 18:29:19 +0530

    updated code.

---
+++ container-module/src/main/java/com/khadri/spring/core/container/config/ContainerConfig.java
@@ -0,0 +1,8 @@
+package com.khadri.spring.core.container.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ContainerConfig {
+
+}
---
+++ container-module/src/main/java/com/khadri/spring/core/container/util/ContainerUtil.java
@@ -0,0 +1,13 @@
+package com.khadri.spring.core.container.util;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.khadri.spring.core.config.DoctorConfig;
+import com.khadri.spring.core.container.config.ContainerConfig;
+
+
+public class ContainerUtil {
+	public static  AnnotationConfigApplicationContext applicationContext() {
+		return new AnnotationConfigApplicationContext(ContainerConfig.class, DoctorConfig.class);
+	}
+}
