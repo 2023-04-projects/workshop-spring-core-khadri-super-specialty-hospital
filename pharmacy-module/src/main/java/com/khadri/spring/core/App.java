3148734344249ae82095fc7f5853595e72bcd02e
khadri2021 <khadri.institutions@gmail.com>
2025-02-25 18:02:20 +0530

    Added modules : doctor and pharmacy

---
+++ pharmacy-module/src/main/java/com/khadri/spring/core/App.java
@@ -0,0 +1,10 @@
+package com.khadri.spring.core;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
